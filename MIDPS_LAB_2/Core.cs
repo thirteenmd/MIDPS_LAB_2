25bfa9197c8eec126cc864c8dccc85ba7150bb20
szraksy <aksoysezer95@gmail.com>
2017-04-12 00:15:50 +0300

    Core module planing

---
+++ MIDPS_LAB_2/Core.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIDPS_LAB_2
+{
+    class Core
+    {
+    }
+}
--- MIDPS_LAB_2/GUI.cs
+++ MIDPS_LAB_2/GUI.cs
@@ -12,9 +12,25 @@ namespace MIDPS_LAB_2
 {
     public partial class Calculator : Form
     {
+        Core core = new Core();
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
         public Calculator()
         {
             InitializeComponent();
         }
+
+
     }
 }
